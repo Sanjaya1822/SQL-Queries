87fb1266a6366e98f5ccfe79c85898186fd2b361
Sanjaya1822 <san9345420@gmail.com>
2026-04-19 22:22:27 +0530

    Create Rising Temperature.sql

---
+++ Rising Temperature.sql
@@ -0,0 +1,7 @@
+Problem : Rising Temperature
+Link : https://leetcode.com/problems/rising-temperature/description/
+
+Solution:
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE w1.recordDate -w2.recordDate = 1 AND w1.temperature > w2.temperature;
