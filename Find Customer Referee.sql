5eb1298ef6abe254ffc630dc99b0698296357205
Sanjaya1822 <san9345420@gmail.com>
2026-04-19 22:27:20 +0530

    Create Find Customer Referee.sql

---
+++ Find Customer Referee.sql
@@ -0,0 +1,5 @@
+Problem: Find Customer Referee
+Link : https://leetcode.com/problems/find-customer-referee/description/
+
+Solution :
+select name from customer where referee_id !=2 or referee_id is null;
