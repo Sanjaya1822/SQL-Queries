fecb21a065a69fc2a1b41e1d6f9f85719d0bbc40
Sanjaya1822 <san9345420@gmail.com>
2026-04-19 22:25:17 +0530

    Create Employee Bonus.sql

---
+++ Employee Bonus.sql
@@ -0,0 +1,10 @@
+Problem : Employee Bonus
+Link : https://leetcode.com/problems/employee-bonus/description/
+
+Solution: 
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b 
+    ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
+ 
