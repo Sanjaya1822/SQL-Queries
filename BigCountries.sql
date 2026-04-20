0f89e62ea7527fea967cb3c3b5a3192b2eb59ccd
Sanjaya1822 <san9345420@gmail.com>
2026-04-20 12:30:39 +0530

    Create BigCountries.sql

---
+++ BigCountries.sql
@@ -0,0 +1,5 @@
+Problem : Big Countries
+Link : https://leetcode.com/problems/big-countries/description/
+
+Solution :
+select name,population,area from world where area>=3000000 or population >=25000000;
