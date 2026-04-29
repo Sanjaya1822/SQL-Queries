01b25e2f90bf955fd36e32a77047a4381b84307d
Sanjaya1822 <san9345420@gmail.com>
2026-04-29 18:04:57 +0530

    Create Recyclable_and_Low_Fat_product.sql

---
+++ Recyclable_and_Low_Fat_product.sql
@@ -0,0 +1,5 @@
+Problem :Recyclable and low fat Product
+Link : https://leetcode.com/problems/recyclable-and-low-fat-products/description/
+
+Solution:
+select product_id from products where low_fats = 'Y' and recyclable = 'Y'; 
